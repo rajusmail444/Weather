8ea1dfe3f4003ef52f9803c597303dfc096642cb
Rajesh Billakanti <rajusmail444@gmail.com>
2023-08-27 17:39:34 -0500

    removed unwanted code in extension

--- Weather/Helpers/Extension.swift
+++ Weather/Helpers/Extension.swift
@@ -18,10 +18,6 @@ extension String {
     func capitalizingFirstLetter() -> String {
       return prefix(1).uppercased() + self.lowercased().dropFirst()
     }
-
-    mutating func capitalizeFirstLetter() {
-      self = self.capitalizingFirstLetter()
-    }
 }
 
 extension View {
